91b5055ba0de169f6bba7ce19a1a1ce66b4591b1
aniketlahase54 <aniketlahase122@gmail.com>
2026-08-12 10:12:51 +0530

    type casting

---
+++ 01-Basics/type_casting.py
@@ -0,0 +1,25 @@
+age = "22"
+
+age = int(age)
+
+print(age)
+print(type(age))
+
+price = "999.50"
+price = float(price)
+
+print(price)
+print(type(price))
+
+number = 100
+number = str(number)
+
+print(number)
+print(type(number))
+
+# Boolean conversion
+value = 1
+result = bool(value)
+
+print(result)
+print(type(result))
