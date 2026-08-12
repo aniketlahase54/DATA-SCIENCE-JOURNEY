693e66f3285ac0733f1e4c3535175eda8dfc3264
aniketlahase54 <aniketlahase122@gmail.com>
2026-08-12 10:07:38 +0530

    data type

---
+++ 01-Basics/data_types.py
@@ -0,0 +1,9 @@
+name = "Aniket"          # str
+age = 22                 # int
+salary = 25000.50        # float
+is_employed = False      # bool
+
+print(type(name))
+print(type(age))
+print(type(salary))
+print(type(is_employed))
